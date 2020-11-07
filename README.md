da083277d0c8a176b1b9b433fbb892f39773b865
Edgar Quiroz <eq24f02@protonma.ch>
2020-11-06 20:46:08 -0600

    Lackin strings

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# projectoCompiladores
+# Proyecto Compiladores
+
+## Análisis léxico
+
+### Compilar
+
+```bash
+$ lex lexer.l
+...
+$ gcc lex.yy.c -o lexer
+...
+```
---
+++ classes.h
@@ -0,0 +1,58 @@
+/* Literals */
+#define ID 0
+#define INT 1
+#define FLOAT 2
+#define IM 3
+#define STRING 4
+#define BOOL 46
+
+/* Operators */
+#define PLUS 5 /* + make number positive */
+#define MINUS 6 /* - make number negative */
+#define NEG 7 /* ! negate bool value */
+#define STAR 8 /* * dereference pointer */
+#define AMP 9 /* & reference variable */
+
+#define DIAG 13 /* / */
+#define REM 14 /* % */
+#define ASSIG 15 /* = */
+
+#define EQ 16 /* == */
+#define NEQ 17 /* != */
+#define LEQ 18 /* <= */
+#define GEQ 19 /* >= */
+#define LE 18 /* < */
+#define GE 19 /* > */
+
+/* Reserved words */
+#define IF 20
+#define ELSE 21
+#define BREAK 22
+#define SWITCH 23
+#define CASE 24
+#define DEFAULT 25
+#define CONTINUE 26
+#define VAR 27
+#define CONST 28
+#define DEFER 29
+#define PACKAGE 30
+#define FUNC 31
+#define RETURN 32
+#define STRUCT 33
+#define FOR 34
+
+/* Separators */
+#define LPAR 35
+#define RPAR 36
+#define LBRA 37
+#define RBRA 38
+#define SC 39
+#define COMMA 40
+#define DOT 41 /* . selector*/
+#define INC 42 /* ++ */
+#define DEC 43 /* -- */
+#define DDOT 44
+
+/* Types */
+#define TYPE 45
+
