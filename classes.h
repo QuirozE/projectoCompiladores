3a79315acef10b6a05bb430179fa22089b74d537
Edgar Quiroz <eq24f02@protonma.ch>
2020-11-07 22:16:56 -0600

    Updating int regex, adding string regex draft, reordering lex classes

--- classes.h
+++ classes.h
@@ -4,54 +4,54 @@
 #define FLOAT 2
 #define IM 3
 #define STRING 4
-#define BOOL 46
+#define BOOL 5
 
 /* Operators */
-#define PLUS 5 /* + make number positive */
-#define MINUS 6 /* - make number negative */
-#define NEG 7 /* ! negate bool value */
-#define STAR 8 /* * dereference pointer */
-#define AMP 9 /* & reference variable */
+#define PLUS 6 /* +  */
+#define MINUS 7 /* - */
+#define INC 8 /* ++ */
+#define DEC 9 /* -- */
+#define NEG 10 /* ! */
+#define STAR 11 /* * */
+#define AMP 12 /* & */
+#define DOT 13 /* . selector*/
 
-#define DIAG 13 /* / */
-#define REM 14 /* % */
-#define ASSIG 15 /* = */
+#define DIAG 14 /* / */
+#define REM 15 /* % */
+#define ASSIG 16 /* = */
 
-#define EQ 16 /* == */
-#define NEQ 17 /* != */
-#define LEQ 18 /* <= */
-#define GEQ 19 /* >= */
-#define LE 18 /* < */
-#define GE 19 /* > */
+#define EQ 17 /* == */
+#define NEQ 18 /* != */
+#define LEQ 19 /* <= */
+#define GEQ 20 /* >= */
+#define LE 21 /* < */
+#define GE 22 /* > */
 
 /* Reserved words */
-#define IF 20
-#define ELSE 21
-#define BREAK 22
-#define SWITCH 23
-#define CASE 24
-#define DEFAULT 25
-#define CONTINUE 26
-#define VAR 27
-#define CONST 28
-#define DEFER 29
-#define PACKAGE 30
-#define FUNC 31
-#define RETURN 32
-#define STRUCT 33
-#define FOR 34
+#define IF 23
+#define ELSE 24
+#define BREAK 25
+#define SWITCH 26
+#define CASE 27
+#define DEFAULT 28
+#define CONTINUE 29
+#define VAR 30
+#define CONST 31
+#define DEFER 32
+#define PACKAGE 33
+#define FUNC 34
+#define RETURN 35
+#define STRUCT 36
+#define FOR 37
 
 /* Separators */
-#define LPAR 35
-#define RPAR 36
-#define LBRA 37
-#define RBRA 38
-#define SC 39
-#define COMMA 40
-#define DOT 41 /* . selector*/
-#define INC 42 /* ++ */
-#define DEC 43 /* -- */
-#define DDOT 44
+#define LPAR 38 /* ( */
+#define RPAR 39 /* ) */
+#define LBRA 40 /* [ */
+#define RBRA 41 /* ] */
+#define SC 42 /* ; */
+#define COMMA 43 /* , */
+#define DDOT 44 /* : */
 
 /* Types */
 #define TYPE 45
